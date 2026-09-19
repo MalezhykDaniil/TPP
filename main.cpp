86e3f8f646626f2cf1d138fecea90bdf3c0844a0
MalezhykDaniil <d.malezhyk_fit_10_24_b_d@knute.edu.ua>
2026-09-19 19:57:41 +0300

    Implement student A algorithm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <memory>
+#include <iomanip>
 #include "shared_types.h"
 
+std::unique_ptr<Result> calculateA(std::shared_ptr<const InputData> data);
+
 int main() {
-	std::cout << "Сикс севен";
-	return 0;
+    auto input = std::make_shared<const InputData>(InputData{
+        .points = { {0.0, 0.0}, {1.0, 1.0}, {2.0, 4.0}, {3.0, 9.0}, {4.0, 16.0} },
+        .x0 = 2.5
+        });
+
+    auto resA = calculateA(input);
+    auto [valA, errA] = *resA;
+  
+    std::cout << "Значення: " << valA << "\n";
+    std::cout << "Оцінка похибки: " << errA << "\n\n";
+
+    return 0;
 }
